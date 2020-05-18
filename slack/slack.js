49c43c8f6bf793ab8db50f5a6185d4b47d5b69f7
Sarah Baines <sarahbaines@sarahs-mbp-2.home>
2020-05-18 17:02:06 +0100

    basic setup of rooms and namespaces

--- slack/slack.js
+++ slack/slack.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const socketio = require('socket.io');
+let namespaces = require('./data/namespaces');
 
 app.use(express.static(__dirname + '/public'));
 
